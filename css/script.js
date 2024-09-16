33e1c5693426485c7cc17b84ca7c3000f2fa30f8
Marylinelesaffre <marylineetnoe@outlook.fr>
2024-09-16 13:21:04 +0200

    menu-burger

---
+++ css/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $(".toggle"). on("click",function () {
+        if ($(".navbar").hasClass("active")) {
+            $(".navbar").removeClass("active")
+            $(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>")
+        } else {
+            $(".navbar").addClass("active");
+            $(this).find("a").html("<ion-icon name='clove-outline'></ion-icon>")
+        }
+        
+    })
+    
+})
+function toggleMenu() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('active');
+}
