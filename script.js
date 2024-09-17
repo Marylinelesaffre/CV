3ab2d0d5d2841c284c4033e826d79afc3d0cd899
Marylinelesaffre <marylineetnoe@outlook.fr>
2024-09-17 16:21:17 +0200

    dark-mode

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ function toggleMenu() {
     const nav = document.querySelector('.nav');
     nav.classList.toggle('active');
 }
+function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
