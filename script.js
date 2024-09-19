5031d7300985c0f2d3f91aaaacafa0796cb76369
Marylinelesaffre <marylineetnoe@outlook.fr>
2024-09-19 14:02:51 +0200

    changement image

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+var element = document.body;
+var images = document.querySelector(".slider img");
+function dark(){
+    if(element.classList.length === 0)
+        {
+            images.src = "img/contact.jpg"
+        }else 
+        { console.log(images)
+            images.src = "img/slider.webp"
+        }
+}
 
 function toggleMenu() {
     const nav = document.querySelector('.nav');
     nav.classList.toggle('active');
 }
+
 function myFunction(){
-    var element = document.body;
+    console.log(element)
     element.classList.toggle("dark-mode");
+    dark()
 }
+
+
 
 
 
