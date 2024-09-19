be67d98f9dbfc917d7848b542feda199c537f8fa
Marylinelesaffre <marylineetnoe@outlook.fr>
2024-09-19 12:38:12 +0200

    padding

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-$(function () {
-    $(".toggleMenu"). on("click",function () {
-        if ($(".navbar").hasClass("active")) {
-            $(".navbar").removeClass("active")
-            $(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>")
-        } else {
-            $(".navbar").addClass("active");
-            $(this).find("a").html("<ion-icon name='clove-outline'></ion-icon>")
-        }
-        
-    })
-    
-})
 
 function toggleMenu() {
     const nav = document.querySelector('.nav');
@@ -20,5 +7,8 @@ function myFunction(){
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+
+
 
 
